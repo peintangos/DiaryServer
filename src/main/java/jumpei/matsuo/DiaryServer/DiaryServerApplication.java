ed729408904c19420ae29047e14a9c45e48a90ba
JumpeiMatsuo <beatsjunpei@gmail.com>
2022-10-03 22:56:21 +0900

    初期コミット

---
+++ src/main/java/jumpei/matsuo/DiaryServer/DiaryServerApplication.java
@@ -0,0 +1,13 @@
+package jumpei.matsuo.DiaryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jumpei/matsuo/DiaryServer/DiaryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jumpei.matsuo.DiaryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
